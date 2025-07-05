68544a33a941d958fc6ed3b8f75e4d8dbe0bf256
Mathushanth <mathusatkunarasa13@gmail.com>
2025-07-05 13:25:47 +0530

    Push latest Code

---
+++ Unicom.DB/MainForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Unicom.DB.Service;
+
+namespace Unicom.DB
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LoginForm loginForm = new LoginForm();
+            loginForm.Show();
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            var adminService = new AdminService();
+            adminService.SeedDefaultAdmin();
+        }
+    }
+}
